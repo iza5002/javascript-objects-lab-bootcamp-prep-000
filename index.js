050cd7f2f46a7c47b0fb3fcf859d1f7c2f6767c0
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-01-21 12:30:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 
 
 function deleteFromObjectByKey(obj, key){
-  
+  var newObj = Object.assign({}, obj);
   delete newObj[key]
   return newObj
 }
